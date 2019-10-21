10451053a7e3a713fed621e4d3bb48f6bf1cfd5e
Theo Bonnard <theo.bonnard@supelec.fr>
2019-10-21 16:37:37 +0200

    CLI and sockets

--- equipement.py
+++ equipement.py
@@ -1,3 +1,5 @@
+import socket
+
 from certificate import X509Certificate
 from key_pair import KeyPair
 
@@ -22,3 +24,28 @@ class Equipment:
         s += "My public key is {}\n".format(self.key_pair.public_key().public_numbers())
         s += "My certificate is:\n{}\n".format(self.certificate)
         return s
+
+    def server(self):
+        print("I play server and wait on port {}".format(self.port))
+        with socket.socket() as s:
+            s.bind(("localhost", self.port))
+            s.listen()
+            conn, addr = s.accept()  # conn is a socket connected to addr
+            with conn:
+                print("Connected by", addr)
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    print("I received: {}".format(data))
+                    conn.sendall(data)
+
+    def client(self, addr: str, port: int):
+        print("I play client and want to acces {} on port {}".format(addr, port))
+        with socket.socket() as s:
+            s.connect((addr, port))
+            s.sendall(b"Hello, world")
+            data = s.recv(1024)
+
+    def show_certs(self):
+        print("TO-DO afficher les certs")
--- main.py
+++ main.py
@@ -4,3 +4,30 @@ name, port = input("<name> <port>\n").split()
 port = int(port)
 
 equipment = Equipment(name, port)
+
+while True:
+    print(">>")
+    s = input()
+    if s == "h":
+        print("q => Quitter")
+        print("i => Informations de l'équipement")
+        print("s => Insertion en tant que server")
+        print("c => Insertion en tant que client")
+        print("r => Liste des équipements sur le réseau domestique")
+    elif s == "q":
+        break
+    elif s == "i":
+        print(equipment)
+    elif s == "s":
+        equipment.server()
+    elif s == "c":
+        server_name, server_port = input("<server_name> <server_port>\n").split()
+        server_port = int(server_port)
+        equipment.client(server_name, server_port)
+    elif s == "r":
+        equipment.show_certs()
+    else:
+        print("Unknown command")
+
+
+print("Main loop finished")
